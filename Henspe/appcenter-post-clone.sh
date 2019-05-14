9181a83b7b40424e7e12391fc5eecc8f79d1f396
Fredrik Kaupang <fredrikkaupang@hotmail.com>
2019-05-14 13:41:17 +0200

    Clone to project folder

--- Henspe/appcenter-post-clone.sh
+++ Henspe/appcenter-post-clone.sh
@@ -3,45 +3,6 @@
 #
 # Downloads the SNLA Core project so that it could be correctly linked at build time
 
-cd $APPCENTER_SOURCE_DIRECTORY
-
 echo $(pwd)
-echo "TESTING THAT WE GOT HERE"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
-echo "*"
 
 git clone https://github.com/snla-system/SNLA-app
