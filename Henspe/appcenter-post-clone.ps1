353880f87649439f4ea6cefabe0ed3ef66b832ce
Fredrik Kaupang <fredrikkaupang@hotmail.com>
2019-05-14 13:23:57 +0200

    Generate .ps1 script too

---
+++ Henspe/appcenter-post-clone.ps1
@@ -0,0 +1,47 @@
+
+#!/usr/bin/env bash
+#
+# Downloads the SNLA Core project so that it could be correctly linked at build time
+
+cd $APPCENTER_SOURCE_DIRECTORY
+
+echo $(pwd)
+echo "TESTING THAT WE GOT HERE"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+echo "*"
+
+git clone https://github.com/snla-system/SNLA-app
