24a5611d4e79e0b9e142685257c4abec19a1135f
Fredrik Kaupang <fredrikkaupang@hotmail.com>
2019-05-14 13:10:31 +0200

    Updated App-center-post clone

--- Henspe/appcenter-post-clone.sh
+++ Henspe/appcenter-post-clone.sh
@@ -3,4 +3,8 @@
 #
 # Downloads the SNLA Core project so that it could be correctly linked at build time
 
+cd $APPCENTER_SOURCE_DIRECTORY
+
+echo $(pwd)
+
 git clone https://github.com/snla-system/SNLA-app
