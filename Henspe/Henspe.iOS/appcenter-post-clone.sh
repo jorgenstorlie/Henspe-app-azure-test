529eed74a1733f52aa41044dfa198c1b067bdbd1
Jørgen Storlie <jorgen.storlie@gmail.com>
2020-06-19 15:18:20 +0200

    Update appcenter-post-clone.sh

--- Henspe/Henspe.iOS/appcenter-post-clone.sh
+++ Henspe/Henspe.iOS/appcenter-post-clone.sh
@@ -2,6 +2,7 @@
 #!/usr/bin/env bash
 #
 # Downloads the SNLA Core project so that it could be correctly linked at build time
+git config --global --add url."git@github.com:".insteadOf "https://github.com/"
 echo $(ls)
 cd "../"
 cd "../"
